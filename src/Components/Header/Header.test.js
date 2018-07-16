3a0dad3d34d5632081199376b0c3ec3823c150f2
Goodness Daramola <gdaramol@thoughtworks.com>
2018-07-16 12:19:55 -0500

    [gd] configuring tests

---
+++ src/Components/Header/Header.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Header from './Header';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
+
+
+describe('Header Component', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<Header hello = {123}/>);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+});
